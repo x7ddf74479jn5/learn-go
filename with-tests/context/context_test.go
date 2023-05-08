3542c395b8873faf041bde6c866c52fca8cc5657
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-08 17:10:34 +0900

    ✨ feat: context

---
+++ with-tests/context/context_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+  "context"
+  "net/http"
+  "net/http/httptest"
+  "testing"
+  "time"
+)
+
+type StubStore struct {
+  response string
+}
+
+func (s *StubStore) Fetch() string {
+  return s.response
+}
+
+func TestServer(t *testing.T) {
+  data := "hello, world"
+
+  t.Run("returns data from store", func(t *testing.T) {
+    store := &SpyStore{response: data, t: t}
+    svr := Server(store)
+
+    request := httptest.NewRequest(http.MethodGet, "/", nil)
+    response := httptest.NewRecorder()
+
+    svr.ServeHTTP(response, request)
+
+    if response.Body.String() != data {
+      t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+    }
+
+  })
+
+  t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+    store := &SpyStore{response: data, t: t}
+    svr := Server(store)
+
+    request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+    cancellingCtx, cancel := context.WithCancel(request.Context())
+    time.AfterFunc(5*time.Millisecond, cancel)
+    request = request.WithContext(cancellingCtx)
+
+    response := &SpyResponseWriter{}
+
+    svr.ServeHTTP(response, request)
+
+    if response.written {
+      t.Error("a response should not have been written")
+    }
+  })
+}
+
+
